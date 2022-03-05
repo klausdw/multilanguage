f7633b4a56c171991b2b70d1344eb53902d44b6a
Klaus Wagner <kwagner@Klauss-MBP-2.fritz.box>
2022-03-05 08:05:23 +0100

    new icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,12 +25,14 @@ const Footer: React.FC = () => {
 		<footer className="w-full py-6 px-5 space-y-6 bg-blue-600">
 			<div className="grid grid-cols-1 gap-y-2 sm:grid-cols-4">
 				<div className="flex justify-center text-white">
-					<Whatsapp />
-					<Facebook />
-					<Instagram />
+					<div className="flex justify-center items-center">
+						<Facebook />
+						<Instagram />
+						<Whatsapp />
+					</div>
 				</div>
 				{footer.map((item) => (
-					<Link key={item.name} href={item.href} prefetch={false}>
+					<Link key={item.name} href={item.href}>
 						<a
 							rel={item.rel}
 							className="flex justify-center items-center text-base font-medium text-white hover:text-gray-400"
--- components/FooterInline.tsx
+++ components/FooterInline.tsx
@@ -1,14 +1,12 @@
-import { useTranslation } from 'next-i18next'
-import Link from 'next/link'
-import { Whatsapp, Facebook, Instagram } from './Icons'
+import { Facebook, Instagram, Whatsapp } from './Icons'
 
 const FooterInline: React.FC = () => {
 	return (
 		<footer className="w-full py-6 px-5 space-y-6 bg-blue-600">
-			<div className="flex justify-center text-white">
-				<Whatsapp />
+			<div className="flex justify-center items-center text-white">
 				<Facebook />
 				<Instagram />
+				<Whatsapp />
 			</div>
 		</footer>
 	)
