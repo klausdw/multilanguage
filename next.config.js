a794ff261669b0e6bc0c5b1a180c5c61c38c77cd
Klaus Wagner <kwagner@2d9005a5-4de1-41eb-937b-df4fd8b8b107.fritz.box>
2022-03-07 00:56:48 +0100

    Bundle Analyzer

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const { i18n } = require('./next-i18next.config')
+const withPlugins = require('next-compose-plugins')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true',
+})
 
 const securityHeaders = [
 	{ key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
 	{ key: 'X-Content-Type-Options', value: 'nosniff' },
 ]
-
-module.exports = {
+const nextConfigs = {
 	i18n,
 	async headers() {
 		return [
@@ -16,37 +19,10 @@ module.exports = {
 			},
 		]
 	},
-
-	// async redirects() {
-	// 	return [
-	// 		{
-	// 			source: '/candidate', // automatically handles all locales
-	// 			destination:
-	// 				'/trabalhar-na-alemanha-como-enfermeiro&enfermeiros-na-alemanha', // automatically passes the locale on
-	// 			locale: false,
-	// 			permanent: false,
-	// 		},
-	// 		{
-	// 			// does not handle locales automatically since locale: false is set
-	// 			source: '/de/candidate',
-	// 			destination: '/de/arbeiten-in-deutschland',
-	// 			locale: false,
-	// 			permanent: false,
-	// 		},
-	// {
-	// 	// this matches '/' since `en` is the defaultLocale
-	// 	source: '/en',
-	// 	destination: '/en/another',
-	// 	locale: false,
-	// 	permanent: false,
-	// },
-	// {
-	// 	// this gets converted to /(en|fr|de)/(.*) so will not match the top-level
-	// 	// `/` or `/fr` routes like /:path* would
-	// 	source: '/(.*)',
-	// 	destination: '/another',
-	// 	permanent: false,
-	// },
-	// 	]
-	// },
+	images: {
+		domains: ['localhost'],
+		deviceSizes: [640, 768, 1024, 1280, 1536],
+	},
 }
+
+module.exports = withPlugins([[withBundleAnalyzer], nextConfigs])
