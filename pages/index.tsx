a91a58d587f1442715aabc689131bcb3f9e27717
Klaus Wagner <kwagner@Klauss-MBP-2.fritz.box>
2022-03-26 22:12:11 +0100

    remove cls, update layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,38 +19,40 @@ const Home: NextPage = () => {
 				hreflang={t('hreflang')}
 				author={t('author')}
 			/>
-			<Image
-				src={enfermeira}
-				alt="Enfermeira logo"
-				loading="lazy"
-				objectFit="cover"
-			/>
-			<div>
-				<h1 className="mt-3 text-xl text-gray-500 sm:mt-5 sm:text-2xl sm:max-w-xl sm:mx-auto md:mt-5 md:text-2xl lg:mx-0">
-					{t('home1')}
-				</h1>
-				<h2 className="mt-3 text-xl text-gray-500 sm:mt-5 sm:text-2xl sm:max-w-xl sm:mx-auto md:mt-5 md:text-2xl lg:mx-0">
-					{t('home2')}
-				</h2>
-				<h3 className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-2xl sm:max-w-xl sm:mx-auto md:mt-5 md:text-2xl lg:mx-0">
-					{t('home3')}
-				</h3>
-				<h4 className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-					{t('home4')}
-				</h4>
-				<div className="flex justify-center space-x-10 mb-10">
-					<a
-						href={t('candidatepath')}
-						className="max-w-md px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-800 md:py-4 md:text-lg md:px-10"
-					>
-						{t('home5')}
-					</a>
-					<a
-						href={t('contactpath')}
-						className="px-8 py-3 border border-ab text-base font-medium rounded-md text-blue-600 bg-white hover:bg-blue-200 md:py-4 md:text-lg md:px-10"
-					>
-						{t('home6')}
-					</a>
+			<h1 className="text-2xl pt-4 px-4 tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl after:absolute after:w-24 after:h-1 after:ml-0 after:bg-blue-600">
+				<span className="block">AB Connection</span>
+			</h1>
+			<div className="mt-4 grid grid-cols-1 lg:grid-cols-2">
+				<Image
+					src={enfermeira}
+					alt="Enfermeira logo"
+					loading="lazy"
+					objectFit="cover"
+				/>
+				<div className="px-8 mt-6">
+					<h1 className="text-xl text-gray-500 sm:text-2xl sm:max-w-xl sm:mx-auto md:text-2xl lg:mx-0">
+						{t('home1')}
+					</h1>
+					<h2 className="mt-3 text-xl text-gray-500 sm:mt-5 sm:text-2xl sm:max-w-xl sm:mx-auto md:mt-5 md:text-2xl lg:mx-0">
+						{t('home2')}
+					</h2>
+					<h3 className="mt-3 text-xl text-gray-500 sm:mt-5 sm:text-2xl sm:max-w-xl sm:mx-auto md:mt-5 md:text-2xl lg:mx-0">
+						{t('home3')}
+					</h3>
+					<div className="flex justify-center items-center space-x-10 mb-10 mt-10 md:space-x-0">
+						<a
+							href={t('candidatepath')}
+							className="max-w-md px-4 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-800 md:py-4 md:px-4 md:text-lg md:items-center md:flex"
+						>
+							{t('home5')}
+						</a>
+						<a
+							href={t('contactpath')}
+							className="px-8 py-3 border border-ab text-base font-medium rounded-md text-blue-600 bg-white hover:bg-blue-200 md:py-4 md:px-4 md:text-lg"
+						>
+							{t('home6')}
+						</a>
+					</div>
 				</div>
 			</div>
 			<Footer />
