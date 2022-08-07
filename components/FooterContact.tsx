8a300df4e18c3a54058bb7cf073596ebc873260a
Klaus Wagner <kwagner@Klauss-MBP-2.fritz.box>
2022-08-07 11:57:31 +0200

    fix width

--- components/FooterContact.tsx
+++ components/FooterContact.tsx
@@ -4,51 +4,53 @@ import { Facebook, Whatsapp, Location } from './Icons'
 const FooterContact = () => {
 	const { t } = useTranslation('contact')
 	return (
-		<div className="mx-4 p-4 px-10 bg-blue-600 shadow rounded sm:mx-10">
-			<div className="flex flex-wrap mx-auto py-4">
-				<Facebook />
-				<Trans
-					i18nKey="contact5"
-					t={t}
-					components={{
-						strong: (
-							<a
-								href={t('contact6')}
-								className="my-auto text-white hover:text-green-400"
-							></a>
-						),
-					}}
-				/>
-			</div>
-			<div className="flex flex-wrap py-4">
-				<Whatsapp />
-				<Trans
-					i18nKey="contact7"
-					t={t}
-					components={{
-						strong: (
-							<a
-								href={t('contact8')}
-								className="my-auto text-white hover:text-green-400"
-							></a>
-						),
-					}}
-				/>
-			</div>
-			<div className="flex flex-wrap py-4">
-				<Location />
-				<Trans
-					i18nKey="contact9"
-					t={t}
-					components={{
-						strong: (
-							<a
-								href={t('contact9')}
-								className="my-auto text-white hover:text-green-400"
-							></a>
-						),
-					}}
-				/>
+		<div className="mx-auto max-w-7xl">
+			<div className="p-4 bg-blue-600 shadow rounded sm:mx-10">
+				<div className="flex flex-wrap mx-auto py-4">
+					<Facebook />
+					<Trans
+						i18nKey="contact5"
+						t={t}
+						components={{
+							strong: (
+								<a
+									href={t('contact6')}
+									className="my-auto text-white hover:text-green-400"
+								></a>
+							),
+						}}
+					/>
+				</div>
+				<div className="flex flex-wrap py-4">
+					<Whatsapp />
+					<Trans
+						i18nKey="contact7"
+						t={t}
+						components={{
+							strong: (
+								<a
+									href={t('contact8')}
+									className="my-auto text-white hover:text-green-400"
+								></a>
+							),
+						}}
+					/>
+				</div>
+				<div className="flex flex-wrap py-4">
+					<Location />
+					<Trans
+						i18nKey="contact9"
+						t={t}
+						components={{
+							strong: (
+								<a
+									href={t('contact9')}
+									className="my-auto text-white hover:text-green-400"
+								></a>
+							),
+						}}
+					/>
+				</div>
 			</div>
 		</div>
 	)
