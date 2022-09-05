3b7fa06128adcb1f225e514e2c0c123f63a8be04
Klaus Wagner <kwagner@Klauss-MacBook-Pro-2.local>
2022-09-05 19:36:13 +0200

    sitemap generator

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPlugins = require('next-compose-plugins')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
 	enabled: process.env.ANALYZE === 'true',
 })
+const webpack = require('webpack')
 
 const securityHeaders = [
 	{ key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
@@ -19,6 +20,34 @@ const nextConfigs = {
 			},
 		]
 	},
+	async rewrites() {
+		return [
+			{
+				source: '/trabalhar-na-alemanha-como-enfermeiro&enfermeiros-na-alemanha',
+				destination: '/candidate',
+			},
+			{
+				source: '/FAQ-perguntas-frequentes',
+				destination: '/faq',
+			},
+			{
+				source: '/nossos-serviços',
+				destination: '/service',
+			},
+			{
+				source: '/sobre-ab-connection',
+				destination: '/about',
+			},
+			{
+				source: '/entre-em-contato',
+				destination: '/contact',
+			},
+			{
+				source: '/kontakt',
+				destination: '/contact',
+			},
+		]
+	},
 	images: {
 		domains: ['localhost'],
 		deviceSizes: [640, 768, 1024, 1280, 1536],
@@ -28,6 +57,13 @@ const nextConfigs = {
 		EMAIL_FROM: process.env.EMAIL_FROM,
 		EMAIL_PASS: process.env.EMAIL_PASS,
 		GOOGLE_ANALYTICS: process.env.GOOGLE_ANALYTICS,
+		WEBSITE_URL: process.env.WEBSITE_URL,
+	},
+	webpack: (config, { isServer }) => {
+		if (isServer) {
+			require('./scripts/sitemap-generator')
+		}
+		return config
 	},
 }
 
