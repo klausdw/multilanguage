5968fc8347fd0b05946ffc416262cffcaf0a21bc
Klaus Wagner <kwagner@Klauss-MacBook-Pro-2.local>
2022-10-06 10:59:50 +0200

    import only svg

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,22 +1,18 @@
 import { useTranslation } from 'next-i18next'
+import Image from 'next/image'
 
 export const Logo: React.FC = () => {
 	const { t } = useTranslation('navbar')
 	return (
-		<a href={t('logohref')} title="Logo">
+		<a href={t('logohref')} title="AB Connection">
 			<span className="sr-only">AB Connection</span>
-			<span>
-				<picture>
-					<source srcSet="/assets/logo.svg" type="image/svg" />
-					<img
-						src="/assets/logo.svg"
-						alt="AB-Connection"
-						loading="lazy"
-						width="33%"
-						height="33%"
-					/>
-				</picture>
-			</span>
+			<Image
+				src="/assets/logo.svg"
+				alt="AB-Connection"
+				loading="lazy"
+				width={50}
+				height={50}
+			/>
 		</a>
 	)
 }
