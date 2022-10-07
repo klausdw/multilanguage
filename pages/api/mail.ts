361d57e0fbe8e51ab50e151fea7f117610442a08
Klaus Wagner <kwagner@Klauss-MacBook-Pro-2.local>
2022-10-07 17:02:06 +0200

    improv mail.ts

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -3,8 +3,7 @@ require("dotenv").config();
 const nodemailer = require('nodemailer')
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	const { nome, email, mensagem } = JSON.parse(req.body)
-	console.log(nome, email, mensagem)
+	const { name, email, message } = JSON.parse(req.body)
 
 	const transporter = nodemailer.createTransport({
 		host: 'smtp.ionos.de',
@@ -24,19 +23,17 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 		to: process.env.EMAIL_TO,
 		bcc: process.env.EMAIL_COPY,
 		replyTo: email,
-		subject: `${nome} - ${email}`,
-		text: mensagem,
-		html: mensagem.replace(/\n/g, '<br/>'),
+		subject: `${name} - ${email}`,
+		text: message,
+		html: message.replace(/\n/g, '<br/>'),
 	}
 
 	await transporter.sendMail(mailOption)
 		.then((result: any) => {
-			console.log(result);
 			result
 			res.status(200).json({ status: 'OK' })
 		})
 		.catch((err: any) => {
-			console.log(err)
 			err instanceof Error ? err.message : err
 		})
 }
