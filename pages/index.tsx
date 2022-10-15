7720693c1ba746f42befb7afcd7af3b5d24396e3
Klaus Wagner <kwagner@Klauss-MacBook-Pro-2.local>
2022-10-15 19:18:36 +0200

    improv next/image and simplify classnames

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,31 +24,28 @@ const Home: NextPage = () => {
 				<div className="fixed right-1 p-2 bottom-1 mr-2 mb-40 lg:mr-10 sm:mb-20 bg-whats-app border-4 border-whats-app rounded-full text-white z-10">
 					<WhatsappChat />
 				</div>
-				<div className="flex justify-end text-blue-600 -mb-10">
+				<div className="flex items-center justify-end text-blue-600 -mb-8 md:-mb-10">
 					<Facebook />
 					<Instagram />
 					<Whatsapp />
 				</div>
-				<h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl after:absolute after:w-24 after:h-1 after:ml-0 after:bg-blue-600">
+				<h1 className="text-2xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-5xl after:absolute after:w-24 after:h-1 after:ml-0 after:bg-blue-600">
 					<span className="block">AB Connection</span>
 				</h1>
 				<div className="mt-4 grid grid-cols-1 lg:grid-cols-2">
-					<Image
-						src={enfermeira}
-						alt="Enfermeira na Alemanha"
-						objectFit="cover"
-						priority
-					/>
-					<div className="px-0 pt-8 sm:px-8 lg:py-0">
-						<h2 className="text-xl text-gray-500 sm:text-2xl md:text-2xl lg:mx-0">
-							{t('home1')}
-						</h2>
-						<h3 className="mt-3 text-xl text-gray-500 sm:mt-5 sm:text-2xl md:mt-5 md:text-2xl lg:mx-0">
-							{t('home2')}
-						</h3>
-						<h4 className="mt-3 text-xl text-gray-500 sm:mt-5 sm:text-2xl md:mt-5 md:text-2xl lg:mx-0">
-							{t('home3')}
-						</h4>
+					<div className="relative h-52 w-full sm:h-96">
+						<Image
+							src={enfermeira}
+							layout="fill"
+							alt="Enfermeira na Alemanha"
+							objectFit="contain"
+							priority
+						/>
+					</div>
+					<div className="space-y-4 px-0 pt-2 sm:px-8 sm:pt-2 lg:pt-6 lg:py-0 xl:pt-0 text-xl text-black sm:text-gray-500 sm:text-2xl md:text-2xl lg:mx-0">
+						<h2>{t('home1')}</h2>
+						<h3>{t('home2')}</h3>
+						<h4>{t('home3')}</h4>
 						<div className="grid grid-cols-1 sm:grid-cols-2 mt-2 space-x-0 sm:space-x-4 space-y-2 sm:space-y-0">
 							<a
 								href={t('candidatepath')}
@@ -63,11 +60,11 @@ const Home: NextPage = () => {
 								{t('home6')}
 							</a>
 						</div>
+						<h5 className="mt-10 mb-0 px-8 xs:px-0 sm:px-0 sm:mb-20 sm:mt-6">
+							{t('whats-app')}
+						</h5>
 					</div>
 				</div>
-				<h5 className="mt-10 mb-40 px-8 xs:px-0 sm:px-0 sm:mb-20 sm:mt-6">
-					{t('whats-app')}
-				</h5>
 			</main>
 			<Footer />
 		</>
