4dc7639ec8b25367b7cce8abebf1c25dd8ef88dc
Klaus Wagner <Klaus.DantasWagner@kimeta.de>
2023-09-20 22:42:33 +0200

    resend api

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -1,39 +1,25 @@
 import { NextApiResponse } from "next";
 import { NextApiRequest } from "next";
+import { Resend } from 'resend';
 
-export const runtime = "edge";
-export const dynamic = "force-dynamic";
-
-const RESEND_API_KEY = process.env.EMAIL_PASS;
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	const { name, email, message } = JSON.parse(req.body);
 
 	try {
-		const response = await fetch("https://api.resend.com/emails", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${RESEND_API_KEY}`,
-			},
-			body: JSON.stringify({
-				from: `Contato do Website <${email}>`,
-				to: [process.env.EMAIL_TO],
-				subject: `${name} - ${email}`,
-				text: message,
-				html: message.replace(/\n/g, '<br/>'),
-			}),
+		const data = await resend.emails.send({
+			from: `Contato do Website <${email}>`,
+			to: [`${process.env.EMAIL_TO}`],
+			subject: `${name} - ${email}`,
+			text: message,
+			html: message.replace(/\n/g, '<br/>'),
 		});
 
-		if (response.ok) {
-			const data = await response.json();
-			res.status(200).json(data);
-		} else {
-			const errorData = await response.json();
-			res.status(response.status).json(errorData);
-		}
+		res.status(200).json(data);
 	} catch (error) {
 		console.error("Error:", error);
+		res.status(400).json(error);
 		res.status(500).json({ error: "Internal Server Error" });
 	}
 };
